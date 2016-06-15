7d4ad654b241f7dd992402457baca36a6f71c86e
PizzaCrust <torchprojectserver@gmail.com>
2016-06-15 20:21:57 +0700

    make patch work correctly lol

--- src/main/java/net/pizzacrust/concrete/mixin/MixinPlayerConnection.java
+++ src/main/java/net/pizzacrust/concrete/mixin/MixinPlayerConnection.java
@@ -5,6 +5,7 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(value = PlayerConnection.class, remap = false)
 public abstract class MixinPlayerConnection implements PacketListenerPlayIn, ITickable {
@@ -13,9 +14,15 @@ public abstract class MixinPlayerConnection implements PacketListenerPlayIn, ITi
     public EntityPlayer player;
 
     @Inject(method = "a(Lnet/minecraft/server/PacketPlayInUseEntity;)V", at = @At(value = "HEAD", remap = false))
-    public void a(PacketPlayInUseEntity var1) {
+    public void a(PacketPlayInUseEntity var1, CallbackInfo callbackInfo) {
         PlayerConnectionUtils.ensureMainThread(var1, this, this.player.x());
-        if (this.player.dead) return; // CraftBukkit
-        if (!this.player.isSpectator()) return; // Spigot
+        if (this.player.dead) {
+            callbackInfo.cancel();  // CraftBukkit
+            return;
+        }
+        if (!this.player.isSpectator()) {
+            callbackInfo.cancel(); // Spigot
+            return;
+        }
     }
 }
